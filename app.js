50e564b9c3ffd1753e2195de2188ac7746526a7b
pasclamah <pascal.amah10@gmai.com>
2024-07-11 05:31:01 +0100

    new update

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
-function app() {
+(function() {
+  "use strict";
+
+  //:::::::::::: MENU ITEMS DROPDOWN ::::::::::::
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("header div a");
+
+  window.addEventListener("scroll", () => {
+    const top = window.scrollY;
+
+    sections.forEach((sec) => {
+      const offset = sec.offsetTop - 150;
+      const height = sec.offsetHeight;
+      const id = sec.getAttribute("id");
+
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          document
+            .querySelector("header div a[href*=" + id + "]")
+            .classList.add("active");
+        });
+      }
+    });
+  });
+
+  // Smooth scroll
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+      const navHeight = document.querySelector("header").offsetHeight;
+
+      window.scrollTo({
+        top: targetSection.offsetTop - navHeight,
+        behavior: "smooth",
+      });
+    });
+  });
+
+
+
   //:::::::::::: MENU ITEMS DROPDOWN ::::::::::::
   const menu = document.querySelector(".nav-links");
   const toggleMenu = document.querySelector(".toggle-menu");
@@ -35,7 +77,7 @@ function app() {
   });
 
 
-  
+
   //:::::::::::: FAQ ITEMS DROPDOWN ::::::::::::
   document.querySelectorAll(".faq-checkbox").forEach((checkbox) => {
     checkbox.addEventListener("change", function () {
@@ -48,8 +90,55 @@ function app() {
       }
     });
   });
-}
 
-app();
 
-// // console.log(menuItems);
+
+  //:::::::::::: SCROLL REVEAL ::::::::::::
+  ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+    scale: 1,
+  });
+
+  ScrollReveal().reveal(".hero-content h1, .section-title, .cta-contents h3", {
+    origin: "top",
+  });
+  ScrollReveal().reveal(
+    ".hero-content .hero-btn, .services-content, .cta-button, .faq-item, .contact-content, .footer-col, .about-content-main, .content-details, .hero-content p, .about-image, .cta-contents p",
+    {
+      origin: "bottom",
+    }
+  );
+
+  ScrollReveal().reveal(".count-box", {
+    origin: "bottom",
+    // reset: true,// Use reset if you want the animation to repeat when you scroll up and down
+    beforeReveal: function (el) {
+      const counter = el.querySelector(".counter");
+      animateCounter(counter);
+    },
+  });
+
+  //:::::::::::: COUNTER ::::::::::::
+  function animateCounter(counter) {
+    const duration = 1000; // duration in milliseconds
+    const target = +counter.getAttribute("data-end");
+    let count = 0;
+    const increment = target / (duration / 16); // approximately 60 frames per second
+
+    const updateCount = () => {
+      count += increment;
+      if (count < target) {
+        counter.innerText = Math.ceil(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCount();
+  }
+
+
+})()
